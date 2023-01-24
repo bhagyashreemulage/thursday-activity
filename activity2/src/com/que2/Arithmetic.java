7397c2f86f8ccb9ae188bffdea0d9e8fa47bd678
Prashant <Prashant@LAPTOP-C2IL98BH>
2023-01-24 20:38:08 +0530

    activity2

---
+++ activity2/src/com/que2/Arithmetic.java
@@ -0,0 +1,21 @@
+package com.que2;
+
+public class Arithmetic {
+	int add(int a,int b)
+	{
+		int sum=a+b;
+		return(sum);
+	}
+
+}
+class Adder extends Arithmetic
+{
+	public int addition(int a, int b)
+	{
+		return add(a,b);
+	}
+}
+
+
+
+
